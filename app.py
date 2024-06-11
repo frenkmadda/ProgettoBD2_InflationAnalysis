d23ff1f13aec15c90ff008dfea57a59ac236731c
Giovanni Donisi <82442402+gdonisi@users.noreply.github.com>
2024-06-11 11:22:52 +0200

    Added plotly

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import json
+from html import escape
+
+import plotly
+import plotly.graph_objs as go
 import pymongo
-from flask import Flask, render_template, send_from_directory, Response
-from markupsafe import escape
+from flask import Flask, render_template, send_from_directory, Response, request
 
 import utils
 
@@ -23,6 +27,49 @@ def serve_prova() -> str:
     return render_template('prova.html', data=data)
 
 
+@app.route('/inflation-by-country', methods=['GET', 'POST'])
+def serve_ibc() -> str:
+    countries = ["Italy", "France"]
+    country = "Italy"
+    if request.method == 'POST':
+        country = request.form['country']
+        inflation_values, years = utils.get_inflation_by_country(global_inflation, country)
+        return render_template('inflation-by-country.html', countries=countries)
+    else:
+        return render_template('inflation-by-country.html', countries=countries)
+
+
+@app.route('/eu')
+def serve_test() -> str:
+    query = {"Country Code": {
+        "$in": ["AUT", "BEL", "BGR", "HRV", "CYP", "CZE", "DNK", "EST", "FIN", "FRA", "DEU", "GRC", "HUN", "IRL", "ITA",
+                "LVA", "LTU", "LUX", "MLT", "NLD", "POL", "PRT", "ROU", "SVK", "SVN", "ESP", "SWE"]}}
+    eu = global_dataset.distinct('Country', query)
+    result = utils.get_food_inflation_eur_per_year(global_dataset, eu)
+    output = {}
+
+    # Riempimento del dizionario con i risultati
+    for doc in result:
+        doc.pop('_id')  # Rimuove la chiave '_id' non necessaria
+        output.update(doc)
+
+    anni = list(output.keys())
+    inflazione = list(output.values())
+
+    # Creazione del grafico
+    data = [
+        go.Scatter(
+            x=anni,
+            y=inflazione,
+            mode='lines+markers',
+            name='Inflazione annuale'
+        )
+    ]
+
+    graph_json = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('eu.html', graphJSON=graph_json)
+
+
 @app.get('/js/<path>')
 def serve_js(path: str) -> Response:
     return send_from_directory('static/js', escape(path))
@@ -33,9 +80,9 @@ def serve_css(path: str) -> Response:
     return send_from_directory('static/css', escape(path))
 
 
-@app.get('/favicon.ico')
+@app.get('/favicon.png')
 def serve_favicon() -> Response:
-    return send_from_directory('static', 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory('static', 'logo.png', mimetype='image/png')
 
 
 @app.errorhandler(404)
